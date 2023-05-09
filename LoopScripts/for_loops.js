879354f1bdf7d93c003a1626bf8eef7ee9315d02
Angel Fraser <Angelaffirms@gmail.com>
2023-05-09 10:33:17 -0400

    attempted while

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,11 @@
+// for ( i = 0; i< 7; i++){
+//     lol = "I love loops"
+//     console.log(lol);
+// }
+
+i = 0
+while (i < 7){
+ i = "I love loops"
+ console.log(i +"",i, i +"",i, i +"",i, i +"");
+
+}
