007bd173ee07d2c41fe669830ab014039e463273
Angel Fraser <Angelaffirms@gmail.com>
2023-05-09 14:23:50 -0400

    kevin's demo

--- ArrayScripts/course_search.js
+++ ArrayScripts/course_search.js
@@ -36,6 +36,60 @@ let courses = [
     }
    ];
 
-function course_search(){
-    let 
+   function searchByCourseId(id){
+    for(let i=0;i<courses.length;i++){
+        let course = courses[i];
+        if(course.CourseId == id){
+            return course;
+        }
+    }
 }
+function getCheapCourses(dollarLimit){
+    let matches = []
+    for(let i=0;i<courses.length;i++){
+        let course = courses[i];
+        if(Number(course.Fee) <= dollarLimit){
+            matches.push(course);
+        }
+    }
+    return matches;
+}
+function getCoursesByLocation(location){
+    let matches = []
+    for(let i=0;i<courses.length;i++){
+        let course = courses[i];
+        if(course.Location === location){
+            matches.push(course);
+        }
+    }
+    return matches;
+}
+function printCourse(course){
+    console.log(`
+        CourseId: "${course.CourseId}",
+        Title: "${course.Title}",
+        Location: "${course.Location}",
+        StartDate: "${course.StartDate}",
+        Fee: "${course.Fee}"
+    `)
+}
+function printCourseListTitles(courseList){
+    for(let i=0;i<courseList.length;i++){
+        console.log(courseList[i].Title);
+    }
+}function printCourseList(courseList){
+    for(let i=0;i<courseList.length;i++){
+        printCourse(courseList[i]);
+    }
+}
+console.log("When does the PROG200 course start?");
+console.log(searchByCourseId("PROG200").StartDate)
+
+console.log("What is the title of the PROJ500 course?");
+console.log(searchByCourseId("PROG200").Title)
+
+console.log("What are the titles of the courses that cost $50 or less?");
+printCourseListTitles(getCheapCourses(50))
+
+console.log("What classes meet in \"Classroom 1\"?");
+printCourseList(getCoursesByLocation("Classroom 1"))
